e9412963bfc6c5ec83aeebd7cc4b94cdb514af2a
FarzamN <frzamn64ml@gmail.com>
2024-09-19 14:47:03 +0500

    home page in progress

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -6,7 +6,7 @@ const Index = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/api/test');
+        const response = await fetch("/api/test");
         const data = await response.json();
         console.log(data);
       } catch (error) {
@@ -17,28 +17,20 @@ const Index = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex justify-center items-center min-h-screen">
-      <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full">
-        <h1 className="text-black text-3xl font-bold mb-4">Product</h1>
-        
-        <div className="border border-gray-200 rounded-lg overflow-hidden">
-          <img 
-            className="w-full h-48 object-cover" 
-            src={fileHomeImage} 
-            alt="Home"
-          />
-          
-          <div className="p-4">
-            <h3 className="font-bold text-xl text-gray-800 text-center mb-3">
-              Welcome to Ebay Importer
+    <div className="container center ">
+      <div className="container-sm home-container mt-3">
+        <h1 className="text-black text-2xl font-bold">Product</h1>
+        <div className="center border rounded-xl mt-3 py-24 bg-white">
+          <img className="homeImage" src={fileHomeImage} alt="" />
+          <article class="text-wrap">
+            <h3 className="font-bold text-black text-center mb-2">
+              Welcome to Ebay importer
             </h3>
-            <p className="text-gray-600 mb-4">
-              New Yorkers are facing the winter chill...
-            </p>
-            <div className="text-center">
-              <Button primary>Add Product</Button>
-            </div>
-          </div>
+            <p className="mb-5">New Yorkers are facing the winter chill...</p>
+          </article>
+          <Button tone="critical" className="bg-green-600" size="micro">
+            Start
+          </Button>
         </div>
       </div>
     </div>
