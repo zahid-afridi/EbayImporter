473de093383d77022d8d71047f0ef85c1efb7fc1
Muhammad Zahid <119036431+zahid-afridi@users.noreply.github.com>
2024-09-20 03:45:12 -0700

    add loadeing

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -9,32 +9,36 @@ import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/400-italic.css";
 
 import { QueryProvider, PolarisProvider } from "./components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { StoreInfo } from "./redux/query/user";
 
 export default function App() {
   const dispatch = useDispatch();
+  const [load,setLoad]=useState(false)
   const StoreData = useSelector((state) => state.StoreInfo.StoreDetail);
   console.log("Store data form redux", StoreData);
 
   useEffect(() => {
     if (!StoreData) {
-      dispatch(StoreInfo());
+      dispatch(StoreInfo(setLoad));
     }
   }, []);
   const pages = import.meta.glob("./pages/**/!(*.test.[jt]sx)*.([jt]sx)", {
     eager: true,
   });
+  
 
   return (
-    <PolarisProvider>
-      <QueryProvider>
-        <NavMenu>
-          <a href="/" rel="home" />
-        </NavMenu>
-        <Routes pages={pages} />
-      </QueryProvider>
-    </PolarisProvider>
+         load ? "Loading...":(
+          <PolarisProvider>
+          <QueryProvider>
+            <NavMenu>
+              <a href="/" rel="home" />
+            </NavMenu>
+            <Routes pages={pages} />
+          </QueryProvider>
+        </PolarisProvider>
+         )
   );
 }
--- web/frontend/redux/query/user.js
+++ web/frontend/redux/query/user.js
@@ -1,15 +1,18 @@
 import { UpdateStoreDetail } from "../Slices/user/UserStoreSlice";
 
-export const StoreInfo = () => {
+export const StoreInfo = (load) => {
   return async (dispatch) => {
     try {
+      load(true)
       const response = await fetch("/api/store/info");
       const data = await response.json();
       if (response.status == 200) {
         console.log("Sotre data form api ", data);
         dispatch(UpdateStoreDetail(data));
       }
+      load(false)
     } catch (error) {
+      load(false)
       console.log(error);
     }
   };
