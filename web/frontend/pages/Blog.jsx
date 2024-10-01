be553e8e61e71b4bf483b13e834982845e1fc867
Muhammad Zahid <119036431+zahid-afridi@users.noreply.github.com>
2024-10-01 07:29:45 -0700

    add some changing

--- web/frontend/pages/Blog.jsx
+++ web/frontend/pages/Blog.jsx
@@ -4,18 +4,18 @@ export default function Blog() {
   return (
     <>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="text-4xl font-bold text-center my-10">Browse by Category</h1>
-        <p className="text-center text-gray-600 mb-5">Select a category to see more related content</p>
+        <h1 className="text-4xl font-bold text-center my-4">Browse by Category</h1>
+        {/* <p className="text-center text-gray-600 mb-5">Select a category to see more related content</p> */}
 
         {/* Categories */}
-        <div className="flex justify-center space-x-4 mb-10">
+        {/* <div className="flex justify-center space-x-4 mb-10">
           <button className="px-4 py-2 bg-black text-white rounded-full">All (20)</button>
           <button className="px-4 py-2 bg-gray-200 text-gray-600 rounded-full">Technology (03)</button>
           <button className="px-4 py-2 bg-gray-200 text-gray-600 rounded-full">Lifestyle (02)</button>
           <button className="px-4 py-2 bg-gray-200 text-gray-600 rounded-full">Travel (05)</button>
           <button className="px-4 py-2 bg-gray-200 text-gray-600 rounded-full">Health (09)</button>
           <button className="px-4 py-2 bg-gray-200 text-gray-600 rounded-full">Culture (01)</button>
-        </div>
+        </div> */}
 
         {/* Blog Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
